0e0f00d5ba89541c234755b184d72264c08b9746
paulovieira <paulovieira@gmail.com>
2016-05-25 18:12:09 +0100

    update

--- database/initialize_db.js
+++ database/initialize_db.js
@@ -1,111 +1,119 @@
+var Path = require("path");
 var Fs = require("fs-extra");
 var JSON5 = require("json5");
-require('shelljs/global');
-var _ = require('underscore');
+var execSync = require("child_process").execSync;
 var Glob = require("glob");
-var Chalk = require('chalk');
+var Chalk = require("chalk");
 
-var dbname = process.argv[2];
-var psqlPath = process.argv[3];
+var internals = {};
 
-if(!dbname){
-	console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " <database_name>");
+internals.dbname = process.argv[2];
+internals.psqlPath = process.argv[3];
+
+if(!internals.dbname){
+	console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " <database_name> [<psql>]");
 	process.exit();
 }
 
 // use the default psql if not given
-// for dev: /home/pvieira/postgres9.5/bin/psql
-if(!psqlPath){
-	psqlPath = "psql";
+// if we are using the beta version, use something like /home/pvieira/postgres-9.6-beta/bin/psql
+if(!internals.psqlPath){
+	internals.psqlPath = "psql";
 }
 
-// the order in the array returned by glob is alphabetical, so we can manually defined the order
-// using numbers in the filename
 
-// first we run the scripts for the tables and functions
-var scripts = [];
-scripts = scripts.concat(Glob.sync("database/10_*/*.sql"));
-scripts = scripts.concat(Glob.sync("database/20_*/*.sql"));
+internals.createTables = function(){
 
+	// the order in the array returned by glob is lexicographic, so we can define the order
+	// using numbers in the filename
+	var scripts = Glob.sync("database/10_*/*.sql");
 
-var psqlTemplate = _.template("<%= psqlPath %> --dbname <%= dbname %> --file=<%= filename %>");
-var command;
+	for(var i=0; i<scripts.length; i++){
+		// call psql in a new shell
+		internals.execPsql(scripts[i]);
+	}
+};
 
-for(var i=0; i<scripts.length; i++){
+internals.createFunctions = function(){
 
-	command = psqlTemplate({ 
-		dbname: dbname, 
-		filename: scripts[i] ,
-		psqlPath: psqlPath
-	});
-	console.log(command)
-	execPsql(command);
-}
+	var scripts = Glob.sync("database/20_*/*.sql");
 
-// now we run the scripts for the initial data (case by case)
-scripts = [];
-scripts = scripts.concat(Glob.sync("database/90_*/*.sql"));
+	for(var i=0; i<scripts.length; i++){
+		// call psql in a new shell
+		internals.execPsql(scripts[i]);
+	}
+};
 
-// populate countries table
+internals.createInitialData = function(){
 
-var script = "./database/90_initial_data/9011_populate_countries.sql";
-command = psqlTemplate({ 
-	dbname: dbname, 
-	filename: script,
-	psqlPath: psqlPath
-});
-execPsql(command);
+	// for the scripts with initial data we should do it case by case
 
+	// a) countries
+	var script = "./database/90_initial_data/9011_populate_countries.sql";
+	internals.execPsql(script);
 
-// populate definitions table; here we read the data from a json file and manually insert;
-// for each object in the json file we create the corresponding psql command; this command is
-// written to a temporary file because the quotes are difficult to handle when we try to execute 
-// directly (using the "--command" options in psql)
+	// b) definitions
 
-var inputData = "./database/90_initial_data/9040_populate_definitions.json";
-var array = JSON5.parse(Fs.readFileSync(inputData, "utf8"));
+	// here we read the initial data from a json file and create a sql file on-the-fly;
+	// data is inserted using the auxiliary "definitions_upsert" function (already defined in one of the previous scripts);
+	// 
+	// for each object in the json file we create the corresponding sql command; example:
+	//
+	//   SELECT * FROM definitions_upsert('{ name: "xyz", age: 10}')
+	//
+	// this command is written to a temporary file, which is then executed with psql as above 
+	// (using the '--file' options)
+	//
+	// we do it this way because the quotes are difficult to handle in the shell (if the sql command was given
+	// directly with '--command' option)
 
-var upsertTemplate = _.template("   SELECT * FROM definitions_upsert('\n<%= inputObj %>\n');   ")
-var tempFile = "./database/90_initial_data/temp.sql";
-var commandStr;
+	var inputData = "./database/90_initial_data/9040_populate_definitions.json";
+	var array = JSON5.parse(Fs.readFileSync(inputData, "utf8"));
 
-for(var i=0; i<array.length; i++){
+	var tempDir = Path.join(__dirname, "temp_" + String(Date.now()).substr(-5));
+	Fs.mkdirsSync(tempDir);
 
-	// write the psql command to a temporary file
-	commandStr = upsertTemplate({inputObj: JSON.stringify(array[i]) });
-	Fs.writeFileSync(tempFile, commandStr);
+	for(i=0; i<array.length; i++){
 
-	// execute the command in the temporary file
-	command = psqlTemplate({ 
-		dbname: dbname, 
-		filename: tempFile,
-		psqlPath: psqlPath
-	});
-	execPsql(command);
-}
+		var tempSql = `
 
-Fs.removeSync(tempFile);
+				SELECT * FROM definitions_upsert('
+					${ JSON.stringify(array[i]) }
+				');
+		`;
 
+		// write the sql command to a file in the temporary dir
+		var tempFile = Path.join(tempDir, `temp_${ i }.sql`);
+		Fs.writeFileSync(tempFile, tempSql);
 
-// if we got it here, everything should be fine 
-console.log(Chalk.green.bold("\n[shelljs] sql scripts ran successfully!"));
-
-
-function execPsql(command){
-
-	var result, line, output;
-	console.log(Chalk.blue.bold("\n[shelljs] executing \'" + command + "\'"));
-
-	result = exec(command);
-
-	if(result.code !== 0){
-		console.log(Chalk.white.bgRed.bold("\n[shelljs] there was error running the sql scripts!"));
-		exit(1);
+		// call psql as before (using a new shell)
+		internals.execPsql(tempFile);
 	}
 
-	output = result.output.split("\n");
+	// "remove" method from fs-extra ("directory can have contents")
+	Fs.removeSync(tempDir);
+
+};
+
+internals.execPsql = function(sqlFile){
+
+	var command = `${ internals.psqlPath } --dbname ${ internals.dbname } --file=${ sqlFile }`;
+	console.log(Chalk.blue.bold("\nexecuting \'" + command + "\'"));
+
+	var output;
+	try{
+		output = execSync(command, { encoding: "utf8" });
+	}
+	catch(err){
+		console.log(Chalk.white.bgYellow.bold(err.message.trim()));
+		console.log(Chalk.white.bgRed.bold("\nthere was error running the sql scripts!"));
+		process.exit(1);
+	}
+
+	// parse the output from psql
+	output = output.split("\n");
 	for(var j=0; j<output.length; j++){
-		line = output[j].toLowerCase();
+		var line = output[j].toLowerCase().trim();
 
 		if(line.indexOf("error") >= 0){
 
@@ -115,8 +123,22 @@ function execPsql(command){
 				continue;
 			}
 
-			console.log(Chalk.white.bgRed.bold("\n[shelljs] there was error running the sql scripts!"));
-			exit(1);	
+			console.log(Chalk.white.bgYellow.bold(line));
+			console.log(Chalk.white.bgRed.bold("\nthere was error running the sql scripts!"));
+			process.exit(1);
 		}
 	}
+
 };
+
+
+// first we run the scripts for the tables and postgres functions
+internals.createTables();
+internals.createFunctions();
+internals.createInitialData()
+
+console.log(Chalk.green.bold("\nsql scripts ran successfully!"));
+
+
+
+

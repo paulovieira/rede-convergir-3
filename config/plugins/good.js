557ab1bd1669e9f2119152eb7bba9c7f4e1dc6b2
paulovieira <paulovieira@gmail.com>
2016-06-29 19:12:13 +0100

    update

--- config/plugins/good.js
+++ config/plugins/good.js
@@ -1,3 +1,5 @@
+var Path = require('path');
+var Config = require('nconf');
 
 /*
 ops - System and process performance - CPU, memory, disk, and other metrics.
@@ -7,24 +9,49 @@ error - request responses that have a status code of 500. This maps to the "requ
 request - Request logging information. This maps to the hapi 'request' event that is emitted via request.log().
 */
 
+
+
 module.exports = {
     ops: {
-        interval: 1000
+        interval: 60*1000
     },
-    
-    reporters: {
-
-        console: [
-            {
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{ log: '*', response: '*', /*error: '*', */request: '*' }]
-            }, 
-            {
-                module: 'good-console'
-            }, 
-            'stdout'
-        ],
-
-    }
+    reporters: {}
 };
+
+// add good reporters, unless they are explicitely turned off
+
+if(Config.get('good-console')!=="false"){
+
+    module.exports.reporters['console'] = [
+        {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ log: '*', response: '*', error: '*', request: '*' }]
+        }, 
+        {
+            module: 'good-console'
+        }, 
+        'stdout'
+    ];
+}
+
+if(Config.get('good-file-ops')!=="false"){
+
+    module.exports.reporters['file-ops'] = [
+        {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ ops: '*' }]
+        }, 
+        {
+            module: 'good-squeeze',
+            name: 'SafeJson',
+            args: [ null, { separator: ',' } ]
+        }, 
+        {
+            module: 'rotating-file-stream',
+            // rotates the file when its size exceeds n KiloBytes
+            args: [ 'ops_log', { size: '10K', path: Path.join(Config.get('rootDir'), 'logs') } ]  
+        }
+    ];
+}
